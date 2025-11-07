f132b46286104481a637e0c38e65a1a088ab617b
asimmetti <asimmetti@gmail.com>
2025-11-07 13:18:34 +0100

    initialised the backend project

---
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/VlaBackendApplication.java
@@ -0,0 +1,13 @@
+package de.vlaorgatu.vlabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlaBackendApplication.class, args);
+	}
+
+}
---
+++ VLA-Backend/src/test/java/de/vlaorgatu/vlabackend/TestVlaBackendApplication.java
@@ -0,0 +1,11 @@
+package de.vlaorgatu.vlabackend;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestVlaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(VlaBackendApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
