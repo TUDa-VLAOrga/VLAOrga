dff9791d4b4eaf7497864d35289e5c8089f88f15
Johannes Jung <johannes.jung@stud.tu-darmstadt.de>
2025-12-01 12:54:41 +0100

    lint: replace all tabs by spaces (backend)

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/VlaBackendApplication.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/VlaBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VlaBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VlaBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VlaBackendApplication.class, args);
+    }
 
 }
--- VLA-Backend/src/test/java/de/vlaorgatu/vlabackend/TestVlaBackendApplication.java
+++ VLA-Backend/src/test/java/de/vlaorgatu/vlabackend/TestVlaBackendApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 
 public class TestVlaBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(VlaBackendApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.from(VlaBackendApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
 
 }
