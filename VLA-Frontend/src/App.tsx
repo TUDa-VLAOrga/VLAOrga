d0122c9ff4f852e65e3bfb11bba9c230a985a35a
Johannes Jung <johannes.jung@stud.tu-darmstadt.de>
2025-12-01 14:34:17 +0100

    lint/frontend: delete unused theme-toggle, appease linter
    move separate component to extra file to appease linter

--- VLA-Frontend/src/App.tsx
+++ VLA-Frontend/src/App.tsx
@@ -1,14 +1,9 @@
 import './App.css'
-import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from './components/mode-toggle';
 
 function App() {
 
   return (
     <div className="App">
-    <ThemeProvider storageKey="vite-ui-theme">
-      <ModeToggle></ModeToggle>
-    </ThemeProvider>
     </div>
   );
 }
