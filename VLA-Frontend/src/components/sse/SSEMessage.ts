733e1d18a57ab15a4d7130017aed75d9062e0e07
Marco Leipnitz <marco.leipnitz@gmx.net>
2025-12-05 16:45:38 +0100

    Added inital example for enum

---
+++ VLA-Frontend/src/components/sse/SSEMessage.ts
@@ -0,0 +1,7 @@
+// Make sure that this enum is kept up to date when changing the API
+
+export enum SSEMessage {
+  EXPERIMENT = "experiment",
+  CALENDAR = "calendar",
+  APPROVEDELETION = "approveDeletion",
+};
