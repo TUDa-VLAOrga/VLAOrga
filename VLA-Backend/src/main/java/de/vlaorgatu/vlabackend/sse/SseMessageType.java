7deb9d3cbc6a5d5b6000caa7fc4d3d24ff8ac6ba
Marco Leipnitz <marco.leipnitz@gmx.net>
2025-12-08 22:42:54 +0100

    Added SseMessageType JavaDoc

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/sse/SseMessageType.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/sse/SseMessageType.java
@@ -5,7 +5,12 @@ package de.vlaorgatu.vlabackend.sse;
  * Make sure that this list is always up-to-date!
  */
 public class SseMessageType {
+    /**
+     * Used for sending Debug messages to the frontend
+     */
     public static final String DEBUG = "DEBUG";
+
+    // TODO: Decide on important SseMessageTypes and think of the syncing events in more detail
     public static final String EXPERIMENT = "experiment";
     public static final String CALENDAR = "calendar";
     public static final String APPROVEDELETION = "approveDeletion";
