85952991d54645c9c031560cd45ce19880e89e1d
Marco Leipnitz <marco.leipnitz@gmx.net>
2025-12-19 12:32:07 +0100

    First draft of frontEnd logger

---
+++ VLA-Frontend/src/components/logger/Logger.ts
@@ -0,0 +1,70 @@
+import type { Dispatch, SetStateAction } from "react";
+import { LogEvent, LogLevel, type LogMessage } from "./LoggerTypes";
+
+export class Logger {
+    /**
+     * Contains all relevant log messages
+     */
+    private static logMessages: LogMessage[] = [];
+    private static loggerReferenceSetter?: Dispatch<SetStateAction<LogMessage[]>>;
+
+    /**
+     * Creates an info log entry for a given message
+     * @param message Message to log
+     * @param event Optional event declaration for improved log filtering
+     */
+    static info(message: string, event?: LogEvent){
+        Logger.customLogMessage(LogLevel.INFO, message, event);
+    }
+
+    /**
+     * Creates an warn log entry for a given message
+     * @param message Message to log
+     * @param event Optional event declaration for improved log filtering
+     */
+    static warn(message: string, event?: LogEvent){
+        Logger.customLogMessage(LogLevel.WARN, message, event);
+    }
+
+    /**
+     * Creates an error log entry for a given message
+     * @param message Message to log
+     * @param event Optional event declaration for improved log filtering
+     */
+    static error(message: string, event?: LogEvent){
+        Logger.customLogMessage(LogLevel.ERROR, message, event);
+    }
+
+    /**
+     * Creates a log message, inserts it into the logger history and updates the Logger component
+     * @param level Level of the log message
+     * @param message The message to log
+     * @param event Optional event declaration for improved log filtering
+     */
+    static customLogMessage(level: LogLevel, message: string, event?: LogEvent){
+        
+        
+        Logger.logMessages.push({
+            date: new Date(),
+            level: level,
+            message: message,
+            eventType: event,
+        });
+
+        let newMessages : LogMessage[] = [...Logger.logMessages];
+        Logger.logMessages = newMessages;
+
+        Logger.updateComponent(newMessages);
+    }
+
+    private static updateComponent(messages: LogMessage[]){
+        // Only log if logging component is ready
+        if(Logger.loggerReferenceSetter)
+            Logger.loggerReferenceSetter(messages);
+    }
+
+    static overrideComponenentMessageSetterRef(setter: Dispatch<SetStateAction<LogMessage[]>>){
+        Logger.loggerReferenceSetter = setter;
+        Logger.info("Updating logger component setter");
+    }
+}
---
+++ VLA-Frontend/src/components/logger/LoggerComponent.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import type { LogMessage } from "./LoggerTypes";
+import { Logger } from "./Logger";
+import "./Logger.css";
+
+function LoggerComponent(){
+    const [logMessages, setLogMessages] = useState<LogMessage[]>([]);
+
+    useEffect(() => {
+        Logger.overrideComponenentMessageSetterRef(setLogMessages);
+    }, []);
+
+    return (
+        <span className="LoggerContainer">
+        
+        <label className="LoggerToggle" htmlFor="LoggerToggler">
+            <input type="checkbox" id="LoggerToggler"></input>
+            Logger
+        </label>
+
+        <div className="Logger">
+
+        {logMessages.map((logMessage, index) => 
+            <div key={"logEntry-" + index} className="LogEntry" data-log-event-type={logMessage.eventType} data-log-level={logMessage.level}>
+                <div className="LogDate">{logMessage.date.toLocaleTimeString()}</div>
+                <div className="LogLevel">[{logMessage.level}]</div>
+                <div className="LogMessage">{logMessage.message}</div>
+            </div>
+        )}
+        </div>
+
+        </span>
+    )
+}
+
+export default LoggerComponent;
