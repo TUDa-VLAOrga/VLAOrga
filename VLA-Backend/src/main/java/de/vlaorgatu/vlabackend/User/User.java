e849b26bafcaace33365cb27d0fc43465dd307a6
asimmetti <asimmetti@gmail.com>
2026-01-07 21:41:16 +0200

    clean up import statements in User classes

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/User/User.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/User/User.java
@@ -1,8 +1,9 @@
 package de.vlaorgatu.vlabackend.User;
 
-import jakarta.persistence.*;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
