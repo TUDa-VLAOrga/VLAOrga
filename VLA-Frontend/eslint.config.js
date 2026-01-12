8abd5076a016e01d8b5459385ff0fb81c3431c65
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-01-12 16:39:54 +0100

    Added enforcment of newline at EOF

--- VLA-Frontend/eslint.config.js
+++ VLA-Frontend/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import stylistic from '@stylistic/eslint-plugin'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -19,6 +20,9 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      '@stylistic': stylistic
+    },
     rules: {
       '@typescript-eslint/no-unused-vars': [
         'error',
@@ -27,6 +31,9 @@ export default defineConfig([
           varsIgnorePattern: '^_',
           caughtErrorsIgnorePattern: '^_',
         },
+      ],
+      '@stylistic/eol-last': [
+        "error", "always"
       ],
     }
   },
