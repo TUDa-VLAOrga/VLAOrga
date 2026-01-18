61e1cad31d3297f7a4d93d6a060fa8f9fb63996c
Johannes Jung <johannes.jung@stud.tu-darmstadt.de>
2026-01-18 22:03:10 +0100

    backend/lecture entity: fix RESTController annotation

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/calendar/lecture/LectureController.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/calendar/lecture/LectureController.java
@@ -3,7 +3,6 @@ package de.vlaorgatu.vlabackend.calendar.lecture;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,12 +10,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
  * REST endpoints for managing {@link Lecture} entities.
  */
-@Controller
+@RestController
 @RequestMapping("/api/calendar/lecture")
 class LectureController {
 
