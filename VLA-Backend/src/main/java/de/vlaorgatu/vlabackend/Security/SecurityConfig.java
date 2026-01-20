6c6c4d8508b34616c2415fddd68cbf0c221b7e49
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-01-20 19:41:46 +0100

    Implemented basic auth

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/Security/SecurityConfig.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/Security/SecurityConfig.java
@@ -5,14 +5,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Set up appropriate http
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // TODO: Should be enabled
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login.html").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login.html")
+                        .loginProcessingUrl("/loginProcessing")
+                        .permitAll()
+                );
+
         return http.build();
     }
 }
