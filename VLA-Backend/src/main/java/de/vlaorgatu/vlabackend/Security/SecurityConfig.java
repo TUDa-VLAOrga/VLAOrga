0f652499459a3e53de93ff11d46dd6a69d6c2b04
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-01-21 12:34:04 +0100

    Added password encoder

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/Security/SecurityConfig.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/Security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package de.vlaorgatu.vlabackend.Security;
 
+import de.vlaorgatu.vlabackend.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -7,18 +9,28 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private UserRepository userRepository;
+
     @Bean
     @Profile("prod")
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 // TODO: Should be enabled
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
+                .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/login.html").permitAll()
                         .anyRequest().authenticated()
                 )
@@ -31,6 +43,13 @@ public class SecurityConfig {
         return http.build();
     }
 
+    // BCrypt seems to be the accepted default, so we are choosing it here as well.
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Unsecure Profile for testing
     @Bean
     @Profile("unsecure")
     SecurityFilterChain unsecureFilterChain(HttpSecurity http) throws Exception {
