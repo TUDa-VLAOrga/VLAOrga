e7ef5dc2bce2e82e4cd475145b04229407747637
Carolin Röll <carolin.roell@stud.tu-darmstadt.de>
2026-01-22 14:51:43 +0100

    add Entity Manager to Configuration

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/linus_connection/LinusAppointmentAutoConfiguration.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/linus_connection/LinusAppointmentAutoConfiguration.java
@@ -1,28 +1,56 @@
 package de.vlaorgatu.vlabackend.linus_connection;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 
 @Configuration
 @PropertySource({"classpath:application.properties"})
 @EnableJpaRepositories(
         basePackages = "de.vlaorgatu.vlabackend.linus_connection",
-        entityManagerFactoryRef = "appointmentEntityManager",
-        transactionManagerRef = "appointmentTransactionManager")
+        entityManagerFactoryRef = "linusAppointmentEntityManager",
+        transactionManagerRef = "linusAppointmentTransactionManager")
 public class LinusAppointmentAutoConfiguration {
+    /*@Autowired
+    private Environment env;*/
+
     @Bean
     @ConfigurationProperties(prefix="spring.second-datasource")
-    public DataSource productDataSource() {
+    public DataSource linusAppointmentDataSource() {
         return DataSourceBuilder.create().build();
     }
 
-    // productEntityManager bean
+    @Bean
+    public LocalContainerEntityManagerFactoryBean linusAppointmentEntityManager() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(linusAppointmentDataSource());
+        //stuff from the tutorial https://www.baeldung.com/spring-data-jpa-multiple-databases that I think is optional
+        /*HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        em.setJpaPropertyMap(properties);*/
+        return em;
+    }
 
-    // productTransactionManager bean
+    @Bean
+    public PlatformTransactionManager linusAppointmentTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(linusAppointmentEntityManager().getObject());
+        return transactionManager;
+    }
 }
