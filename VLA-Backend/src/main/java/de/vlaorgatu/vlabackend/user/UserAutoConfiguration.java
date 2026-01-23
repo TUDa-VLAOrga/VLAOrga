7538b121d36867371f82b6abf3e08668ee5cfac8
Carolin Röll <carolin.roell@stud.tu-darmstadt.de>
2026-01-23 11:50:16 +0100

    add user configuration

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/user/UserAutoConfiguration.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/user/UserAutoConfiguration.java
@@ -1,9 +1,10 @@
-package de.vlaorgatu.vlabackend.linus_connection;
+package de.vlaorgatu.vlabackend.user;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -12,34 +13,37 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import javax.sql.DataSource;
 
-
 @Configuration
 @PropertySource({"classpath:application.properties"})
 @EnableJpaRepositories(
-        basePackages = "de.vlaorgatu.vlabackend.linus_connection",
-        entityManagerFactoryRef = "linusAppointmentEntityManager",
-        transactionManagerRef = "linusAppointmentTransactionManager")
-public class LinusAppointmentAutoConfiguration {
+        basePackages = "de.vlaorgatu.vlabackend.user",
+        entityManagerFactoryRef = "userEntityManager",
+        transactionManagerRef = "userTransactionManager")
+public class UserAutoConfiguration {
+    @Primary
     @Bean
-    @ConfigurationProperties(prefix="spring.second-datasource")
-    public DataSource linusAppointmentDataSource() {
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource userDataSource() {
         return DataSourceBuilder.create().build();
     }
 
+    @Primary
     @Bean
-    public LocalContainerEntityManagerFactoryBean linusAppointmentEntityManager() {
+    public LocalContainerEntityManagerFactoryBean userEntityManager() {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(linusAppointmentDataSource());
-        em.setPackagesToScan("de.vlaorgatu.vlabackend.linus_connection");
+        em.setDataSource(userDataSource());
+        em.setPackagesToScan("de.vlaorgatu.vlabackend.user");
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
         return em;
     }
 
+    @Primary
     @Bean
-    public PlatformTransactionManager linusAppointmentTransactionManager() {
+    public PlatformTransactionManager userTransactionManager() {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(linusAppointmentEntityManager().getObject());
+        transactionManager.setEntityManagerFactory(userEntityManager().getObject());
         return transactionManager;
     }
 }
+
