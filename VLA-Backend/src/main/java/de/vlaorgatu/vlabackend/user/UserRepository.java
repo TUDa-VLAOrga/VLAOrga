af1d6df048d51f173dfe127c3c034130bc93cf45
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-01-26 18:00:54 +0100

    Add interface comment

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/user/UserRepository.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/user/UserRepository.java
@@ -1,15 +1,18 @@
 package de.vlaorgatu.vlabackend.user;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.swing.text.html.Option;
-import java.util.Optional;
 
 /**
  * Repository for accessing and managing {@link User} entities.
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    /**
+     * Retrieve a user by their username.
+     * @param name The unique name to search for
+     * @return The User (if exists) based on the name that is provided
+     */
     Optional<User> findUserByName(String name);
 }
