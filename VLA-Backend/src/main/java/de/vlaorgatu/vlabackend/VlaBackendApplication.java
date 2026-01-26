ea30ce2dae8f672936878d640f93cfc77242c57f
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-01-26 16:48:03 +0100

    Remove unused import

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/VlaBackendApplication.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/VlaBackendApplication.java
@@ -2,7 +2,6 @@ package de.vlaorgatu.vlabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Main Application class.
