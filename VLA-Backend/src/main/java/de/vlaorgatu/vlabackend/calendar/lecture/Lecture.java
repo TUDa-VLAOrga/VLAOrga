97822d97bd34db527aafc0ca289371fac35baec7
Johannes Jung <johannes.jung@stud.tu-darmstadt.de>
2026-02-02 16:06:54 +0100

    backend/linter: forbid unused imports

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/calendar/lecture/Lecture.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/calendar/lecture/Lecture.java
@@ -3,12 +3,10 @@ package de.vlaorgatu.vlabackend.calendar.lecture;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * Represents a lecture in a certain semester. For example "Physik I, WS 25/26".
