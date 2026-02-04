feb2f7d755324a72c0fc378027625e961e6b3e26
Johannes Jung <johannes.jung@stud.tu-darmstadt.de>
2026-02-04 14:08:10 +0100

    lint: remove unused imports

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/user/UserController.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/user/UserController.java
@@ -2,11 +2,8 @@ package de.vlaorgatu.vlabackend.user;
 
 import java.util.List;
 import java.util.Optional;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
