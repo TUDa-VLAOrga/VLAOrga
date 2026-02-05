a723b88f12861e7a38dd93ecc237d7395aa19abc
Johannes Jung <johannes.jung@stud.tu-darmstadt.de>
2026-02-05 17:17:52 +0100

    backend/entities/appointmentCategories: refactor and override Controller REST methods

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/calendar/appointmentcategory/AppointmentCategoryRepository.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/calendar/appointmentcategory/AppointmentCategoryRepository.java
@@ -1,11 +1,11 @@
 package de.vlaorgatu.vlabackend.calendar.appointmentcategory;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * Repository for accessing and managing {@link AppointmentCategory} entities.
  */
 @Repository
-public interface AppointmentCategoryRepository extends CrudRepository<AppointmentCategory, Long> {
+public interface AppointmentCategoryRepository extends JpaRepository<AppointmentCategory, Long> {
 }
