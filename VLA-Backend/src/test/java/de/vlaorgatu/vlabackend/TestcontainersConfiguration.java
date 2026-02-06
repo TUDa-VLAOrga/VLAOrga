77c435374e61cda7fbb10dac7766d54a71ebcbb3
Carolin Röll <carolin.roell@stud.tu-darmstadt.de>
2026-02-06 16:10:42 +0100

    add mariadb to test environment

--- VLA-Backend/src/test/java/de/vlaorgatu/vlabackend/TestcontainersConfiguration.java
+++ VLA-Backend/src/test/java/de/vlaorgatu/vlabackend/TestcontainersConfiguration.java
@@ -3,6 +3,8 @@ package de.vlaorgatu.vlabackend;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistrar;
+import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -15,4 +17,23 @@ class TestcontainersConfiguration {
         return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
     }
 
+    @Bean
+    @ServiceConnection
+    MariaDBContainer<?> mariaDbContainer() {
+        return new MariaDBContainer<>(DockerImageName.parse("mariadb:10.5.29"));
+    }
+
+    @Bean
+    public DynamicPropertyRegistrar databaseProperties(
+            MariaDBContainer<?> mariadbContainer, PostgreSQLContainer<?> postgreSqlContainer) {
+        return (properties) -> {
+            // Connect our Spring application to our Testcontainers instances
+            properties.add("spring.second-datasource.jdbcUrl", mariadbContainer::getJdbcUrl);
+            properties.add("spring.second-datasource.username", mariadbContainer::getUsername);
+            properties.add("spring.second-datasource.password", mariadbContainer::getPassword);
+            properties.add("spring.datasource.jdbcUrl", postgreSqlContainer::getJdbcUrl);
+            properties.add("spring.datasource.username", postgreSqlContainer::getUsername);
+            properties.add("spring.datasource.password", postgreSqlContainer::getPassword);
+        };
+    }
 }
