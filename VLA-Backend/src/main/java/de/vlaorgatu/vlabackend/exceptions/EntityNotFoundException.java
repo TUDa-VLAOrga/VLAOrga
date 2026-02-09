70baaea2337701fee9f6be25a9c9ee8593bca540
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-02-09 12:53:11 +0100

    Status refactor for resource not found

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/exceptions/EntityNotFoundException.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/exceptions/EntityNotFoundException.java
@@ -6,13 +6,13 @@ import org.springframework.web.server.ResponseStatusException;
 /**
  * Custom exception signaling that an entity was not found.
  */
-public class EntityNotFoundException extends ResponseStatusException {
+public class EntityNotFoundException extends ResourceNotFoundException {
     /**
      * Constructor that sets a custom error message.
      *
      * @param reason The error message.
      */
     public EntityNotFoundException(String reason) {
-        super(HttpStatus.NOT_FOUND, reason);
+        super(reason);
     }
 }
