f4a48aed655f06c04a9fdd0f5c008c095a40c77b
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-02-16 15:07:27 +0100

    Remove testing components from calendar view

--- VLA-Frontend/src/pages/CalendarPage.tsx
+++ VLA-Frontend/src/pages/CalendarPage.tsx
@@ -1,8 +1,5 @@
 import CalendarView from "../components/calendar/CalendarView";
-
 import SSEComponent from "@/components/sse/SseComponent";
-import SseFetchTestComponent from "@/components/sse/SseFetchTestComponent";
-import SseTestComponent from "@/components/sse/SseTestComponent";
 
 
 export default function CalendarPage() {
@@ -11,8 +8,6 @@ export default function CalendarPage() {
       <div>
         <CalendarView />
       </div>
-      <SseTestComponent></SseTestComponent>
-      <SseFetchTestComponent></SseFetchTestComponent>
       <SSEComponent></SSEComponent>
     </>
   );
