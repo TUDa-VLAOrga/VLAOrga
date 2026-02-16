0874aa01a66d336e95ce5716af660ccc9a35ceef
Carolin Röll <carolin.roell@stud.tu-darmstadt.de>
2026-02-16 15:37:39 +0100

    Add linusExperiment

---
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/linusconnection/LinusExperimentRepository.java
@@ -0,0 +1,7 @@
+package de.vlaorgatu.vlabackend.linusconnection;
+
+/**
+ * Repository for accessing and managing {@link LinusExperiment} entities.
+ */
+public interface LinusExperimentRepository extends ReadOnlyRepository<LinusExperiment, Long> {
+}
