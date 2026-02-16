d7f1d221a2fc498961250c4153c3eca256bc5d49
Carolin Röll <carolin.roell@stud.tu-darmstadt.de>
2026-02-16 15:36:57 +0100

    Add linusUser

---
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/linusconnection/LinusUser.java
@@ -0,0 +1,49 @@
+package de.vlaorgatu.vlabackend.linusconnection;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+/**
+ * Represents a linus user.
+ */
+@Getter
+@Entity
+// table and column names are taken from https://git.rwth-aachen.de/datenbank-physik/datenbank-physik/-/blob/master/src/Entity/Reservation.php
+// Integer is used for nullable columns and int for non-null ones.
+@Table(name = "user")
+public class LinusUser {
+    /**
+     * Primary key.
+     */
+    @Id
+    private int id;
+
+    /**
+     * Name of the user.
+     */
+    @Column(name = "name")
+    private String name;
+
+    /**
+     * Roles of the user, json format.
+     */
+    @Column(name = "roles")
+    private String roles;
+
+    /**
+     * Hashed Password of the user.
+     */
+    @Column(name = "password")
+    private String password;
+
+    /**
+     * Email adress of the user.
+     */
+    @Column(name = "email")
+    private String email;
+}
---
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/linusconnection/LinusUserRepository.java
@@ -0,0 +1,7 @@
+package de.vlaorgatu.vlabackend.linusconnection;
+
+/**
+ * Repository for accessing and managing {@link LinusUser} entities.
+ */
+public interface LinusUserRepository extends ReadOnlyRepository<LinusUser, Long> {
+}
