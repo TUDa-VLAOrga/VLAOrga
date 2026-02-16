1f743dced0ee2b2072e5c3678aad1c31ec9798fb
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-02-16 13:15:11 +0100

    Temporary not working state

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/linusconnection/LinusExperimentBooking.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/linusconnection/LinusExperimentBooking.java
@@ -5,13 +5,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * Represents that an experiment is booked for an appointment.
  */
 @Getter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 // table and column names are taken from https://git.rwth-aachen.de/datenbank-physik/datenbank-physik/-/blob/master/src/Entity/ReservationExperiment.php
 // Integer is used for nullable columns and int for non-null ones.
 @Table(name = "reservation_experiment")
--- VLA-Backend/src/test/java/de/vlaorgatu/vlabackend/BookingServiceTest.java
+++ VLA-Backend/src/test/java/de/vlaorgatu/vlabackend/BookingServiceTest.java
@@ -1,40 +1,43 @@
 package de.vlaorgatu.vlabackend;
 
-import de.vlaorgatu.vlabackend.databaseSync.LinusSyncService;
-import de.vlaorgatu.vlabackend.entities.calendar.experimentbooking.ExperimentBooking;
-import de.vlaorgatu.vlabackend.entities.calendar.experimentbooking.ExperimentBookingRepository;
 import de.vlaorgatu.vlabackend.linusconnection.LinusExperimentBooking;
 import de.vlaorgatu.vlabackend.linusconnection.LinusExperimentBookingRepository;
 import jakarta.persistence.EntityManager;
-import org.aspectj.lang.annotation.Before;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Import(TestConfiguration.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BookingServiceTest {
+    @Autowired
+    private EntityManager ;
+
     @Autowired
     private LinusExperimentBookingRepository linusbookings;
 
     @BeforeAll
-    void populateLinusOnce() {
-        linusbookings.
+    void setup(){
+        LinusExperimentBooking booking = new LinusExperimentBooking(0,0,0,0,0, LocalDateTime.MIN);
+        linusEntityManager.persist(booking);
+        linusEntityManager.flush();
     }
 
-    @BeforeAll
-    void setup(){
-        vlaexperiments.deleteAll();
-        linusexperiments.deleteAll();
+    @Test
+    void checkPresence(){
+        assertEquals(1, linusbookings.findAll().size());
     }
 }
-
-@Repository
-interface TestLinusExperimentBookingRepository
-        extends CrudRepository<LinusExperimentBooking, Long> {}
