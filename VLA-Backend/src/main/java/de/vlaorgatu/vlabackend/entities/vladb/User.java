c9dfac0bc6f896cd3651dfd0bed51c5bf2f189c3
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-02-16 19:44:56 +0100

    Added auto generation of ids and password colum

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/entities/vladb/User.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/entities/vladb/User.java
@@ -1,10 +1,6 @@
 package de.vlaorgatu.vlabackend.entities.vladb;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 /**
@@ -19,7 +15,7 @@ public class User {
      * Primary key.
      */
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     /**
