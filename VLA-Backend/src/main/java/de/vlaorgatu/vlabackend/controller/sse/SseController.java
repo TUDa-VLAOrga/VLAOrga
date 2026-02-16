1fe4c251211c46646445ba1177b3ae28d4e69ec4
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-02-16 14:33:47 +0100

    Fix merging errors

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/controller/sse/SseController.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/controller/sse/SseController.java
@@ -90,7 +90,7 @@ public class SseController {
             try {
                 connection.send(SseEmitter.event().name(SseMessageType.DEBUG.getValue())
                     .data("SSE Update to all registered connections!"));
-                connection.send(SseEmitter.event().name(SseMessageType.SSEDEBUG)
+                connection.send(SseEmitter.event().name(SseMessageType.SSEDEBUG.getValue())
                     .data("Breaking news! We have received a SSEDEBUG event"));
             } catch (IOException e) {
                 // Broken Pipe Error
