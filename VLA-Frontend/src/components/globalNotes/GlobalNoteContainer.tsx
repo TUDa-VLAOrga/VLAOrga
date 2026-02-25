47cb89d868bd790580911e267fbd984c4be28db9
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-02-25 16:33:12 +0100

    Red dot on note present

--- VLA-Frontend/src/components/globalNotes/GlobalNoteContainer.tsx
+++ VLA-Frontend/src/components/globalNotes/GlobalNoteContainer.tsx
@@ -64,6 +64,12 @@ export default function GlobalNoteContainer() {
         onClick={() => setViewVisible(!viewVisible)}
         style={{display: viewVisible ? "none" : ""}}
       >
+      {globalNotes.length !== 0 &&
+      <>
+        <div className="globalNoteDot"></div>
+      </>
+      }
+      
       </div>
       <div className="globalNoteContainer" style={{display: viewVisible ? "" : "none"}}>
         <div className="globalNoteView">
