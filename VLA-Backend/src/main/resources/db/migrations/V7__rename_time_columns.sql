e75c03f87eaffaf3474ab4bb2b8f79e6de1b9d0e
Johannes Jung <johannes.jung@stud.tu-darmstadt.de>
2026-02-27 13:32:45 +0100

    backend: rename time columns to avoid reserved SQL keyword end

---
+++ VLA-Backend/src/main/resources/db/migrations/V7__rename_time_columns.sql
@@ -0,0 +1,36 @@
+-- auto-generated migration by JPABuddy / flyway in IntelliJ
+ALTER TABLE acceptances
+    ADD end_time TIMESTAMP WITHOUT TIME ZONE;
+
+ALTER TABLE acceptances
+    ADD start_time TIMESTAMP WITHOUT TIME ZONE;
+
+ALTER TABLE appointments
+    ADD end_time TIMESTAMP WITHOUT TIME ZONE;
+
+ALTER TABLE appointments
+    ADD start_time TIMESTAMP WITHOUT TIME ZONE;
+
+ALTER TABLE acceptances
+    ALTER COLUMN end_time SET NOT NULL;
+
+ALTER TABLE acceptances
+    ALTER COLUMN start_time SET NOT NULL;
+
+ALTER TABLE appointments
+    ALTER COLUMN end_time SET NOT NULL;
+
+ALTER TABLE appointments
+    ALTER COLUMN start_time SET NOT NULL;
+
+ALTER TABLE acceptances
+    DROP COLUMN "end";
+
+ALTER TABLE acceptances
+    DROP COLUMN start;
+
+ALTER TABLE appointments
+    DROP COLUMN "end";
+
+ALTER TABLE appointments
+    DROP COLUMN start;
