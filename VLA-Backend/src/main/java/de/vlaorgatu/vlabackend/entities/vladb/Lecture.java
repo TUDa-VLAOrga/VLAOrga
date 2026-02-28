748fb8a72d1824dc3fa488da7414c91859927fb3
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-02-28 15:13:19 +0100

    Remove unused imports

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/entities/vladb/Lecture.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/entities/vladb/Lecture.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Table;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
