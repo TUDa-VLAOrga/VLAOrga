f2bc832e76f1ec1a41207925b9ae3b8df5b6a882
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-03-04 08:48:42 +0100

    Resolve merge conflict in TestUtils

--- VLA-Backend/src/test/java/de/vlaorgatu/vlabackend/TestUtils.java
+++ VLA-Backend/src/test/java/de/vlaorgatu/vlabackend/TestUtils.java
@@ -1,27 +1,11 @@
 package de.vlaorgatu.vlabackend;
 
-<<<<<<< HEAD
-import de.vlaorgatu.vlabackend.repositories.linusdb.LinusAppointmentRepository;
-import de.vlaorgatu.vlabackend.repositories.linusdb.LinusExperimentBookingRepository;
-import de.vlaorgatu.vlabackend.repositories.linusdb.LinusExperimentRepository;
-import de.vlaorgatu.vlabackend.repositories.vladb.AppointmentCategoryRepository;
-import de.vlaorgatu.vlabackend.repositories.vladb.AppointmentMatchingRepository;
-import de.vlaorgatu.vlabackend.repositories.vladb.AppointmentRepository;
-import de.vlaorgatu.vlabackend.repositories.vladb.AppointmentSeriesRepository;
-import de.vlaorgatu.vlabackend.repositories.vladb.ExperimentBookingRepository;
-=======
->>>>>>> main
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.metamodel.EntityType;
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.Set;
-import org.springframework.beans.factory.annotation.Autowired;
-=======
-import java.util.Set;
->>>>>>> main
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,88 +15,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class TestUtils {
     /**
-<<<<<<< HEAD
-     * Repository for the appropriate entity.
-     */
-    @Autowired
-    private LinusAppointmentRepository linusAppointmentRepository;
-
-    /**
-     * Repository for the appropriate entity.
-     */
-    @Autowired
-    private LinusExperimentBookingRepository linusExperimentBookingRepository;
-
-    /**
-     * Repository for the appropriate entity.
-     */
-    @Autowired
-    private LinusExperimentRepository linusExperimentRepository;
-
-    /**
-     * Repository for the appropriate entity.
-     */
-    @Autowired
-    private AppointmentMatchingRepository appointmentMatchingRepository;
-
-    /**
-     * Repository for the appropriate entity.
-     */
-    @Autowired
-    private AppointmentCategoryRepository appointmentCategoryRepository;
-
-    /**
-     * Repository for the appropriate entity.
-     */
-    @Autowired
-    private AppointmentSeriesRepository appointmentSeriesRepository;
-
-    /**
-     * Repository for the appropriate entity.
-     */
-    @Autowired
-    private AppointmentRepository appointmentRepository;
-
-    /**
-     * Repository for the appropriate entity.
-     */
-    @Autowired
-    private ExperimentBookingRepository experimentBookingRepository;
-
-    /**
      * Entity manager for manipulating read-only linus-db.
      */
     @PersistenceContext(unitName = "linusEntityManagerFactory")
     private EntityManager linusEntityManager;
 
     /**
-=======
->>>>>>> main
      * Entity manager for manipulating the vla db.
      */
     @PersistenceContext(unitName = "vlaEntityManagerFactory")
     private EntityManager vlaEntityManager;
-
-    /**
-<<<<<<< HEAD
-     * Deletes all entities in linus.
-     */
-    @Transactional("linusTransactionManager")
-    public void clearLinusDb() {
-        Set<EntityType<?>> entities = linusEntityManager.getMetamodel().getEntities();
-
-        for (EntityType<?> entityType : entities) {
-            Class<?> javaType = entityType.getJavaType();
-            if (javaType.isAnnotationPresent(Entity.class)) {
-                String entityName = entityType.getName();
-                linusEntityManager.createQuery("DELETE FROM " + entityName).executeUpdate();
-            }
-        }
-=======
-     * Entity manager for manipulating the linus db.
-     */
-    @PersistenceContext(unitName = "linusEntityManagerFactory")
-    private EntityManager linusEntityManager;
 
     /**
      * Disables foreign key checks for Vla db.
@@ -148,16 +60,10 @@ public class TestUtils {
         linusEntityManager.createNativeQuery(
                 "SET FOREIGN_KEY_CHECKS = 1;")
             .executeUpdate();
->>>>>>> main
     }
 
     /**
      * Deletes all entities in the vla db.
-<<<<<<< HEAD
-     */
-    @Transactional("vlaTransactionManager")
-    public void clearVlaDb() {
-=======
      * This should only be called in a sequential testing context!
      */
     @Transactional("vlaTransactionManager")
@@ -167,7 +73,6 @@ public class TestUtils {
 
         vlaDisableFkChecks();
 
->>>>>>> main
         Set<EntityType<?>> entities = vlaEntityManager.getMetamodel().getEntities();
 
         for (EntityType<?> entityType : entities) {
@@ -177,18 +82,22 @@ public class TestUtils {
                 vlaEntityManager.createQuery("DELETE FROM " + entityName).executeUpdate();
             }
         }
-<<<<<<< HEAD
+
+        vlaEnableFkChecks();
     }
 
     /**
      * Populates linus with the specified entities.
      * Should be Linus entities
-     * If you get database errors, check that they are inserted according to their dependencies.
      */
     @Transactional("linusTransactionManager")
     void populateLinusDb(ArrayList<Object> linusEntities) {
+        linusDisableFkChecks();
+
         linusEntities.forEach(linusEntityManager::persist);
         linusEntityManager.flush();
+
+        linusEnableFkChecks();
     }
 
     /**
@@ -197,9 +106,10 @@ public class TestUtils {
      */
     @Transactional("vlaTransactionManager")
     void populateVlaDb(ArrayList<Object> vlaEntities) {
+        vlaDisableFkChecks();
+
         vlaEntities.forEach(vlaEntityManager::persist);
         vlaEntityManager.flush();
-=======
 
         vlaEnableFkChecks();
     }
@@ -214,9 +124,9 @@ public class TestUtils {
         linusEntityManager.flush();
         linusEntityManager.clear();
 
-        Set<EntityType<?>> entities = linusEntityManager.getMetamodel().getEntities();
-
         linusDisableFkChecks();
+
+        Set<EntityType<?>> entities = linusEntityManager.getMetamodel().getEntities();
 
         for (EntityType<?> entityType : entities) {
             Class<?> javaType = entityType.getJavaType();
@@ -227,6 +137,5 @@ public class TestUtils {
         }
 
         linusEnableFkChecks();
->>>>>>> main
     }
 }
