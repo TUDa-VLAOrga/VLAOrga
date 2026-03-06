39b54b652d555a34b7aff16f76dd83ccd6ce924a
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-03-06 16:57:11 +0100

    Remove unused import

--- VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/entities/vladb/Appointment.java
+++ VLA-Backend/src/main/java/de/vlaorgatu/vlabackend/entities/vladb/Appointment.java
@@ -1,6 +1,5 @@
 package de.vlaorgatu.vlabackend.entities.vladb;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.CascadeType;
