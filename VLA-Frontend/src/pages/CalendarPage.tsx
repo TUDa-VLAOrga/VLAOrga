3d503d3f9182fb8e297d942f08b476ec120e7688
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-03-06 10:24:18 +0100

    Add centralized z-index file

--- VLA-Frontend/src/pages/CalendarPage.tsx
+++ VLA-Frontend/src/pages/CalendarPage.tsx
@@ -1,6 +1,7 @@
 import GlobalNoteContainer from "@/components/globalNotes/GlobalNoteContainer";
 import CalendarView from "../components/calendar/CalendarView";
 import SSEComponent from "@/components/sse/SseComponent";
+import "@/styles/zIndexes.css"
 
 
 export default function CalendarPage() {
