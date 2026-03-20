198a3f2f182c27b8b4b37161497af786c93920a6
Carolin Röll <carolin.roell@stud.tu-darmstadt.de>
2026-03-20 15:53:24 +0100

    separate symbol if notes are present

--- VLA-Frontend/src/components/calendar/EventDetails/EventDetails.tsx
+++ VLA-Frontend/src/components/calendar/EventDetails/EventDetails.tsx
@@ -189,9 +189,13 @@ export default function EventDetails({
                           onClick={() => setSelectedPersonId(person.id)}
                           aria-label={`Details zu ${people.find((p) => p.id == selectedPersonId)?.name} anzeigen`}
                           title="Details anzeigen"
-                          style={{opacity: (people.find((p) => p.id == person.id)?.notes) === "" ? 0.3 : 0.8}}
                         >
                         </button>
+                        { people.find((p) => p.id == person.id)?.notes !== "" && (
+                          <span className="cv-notesIcon">
+                            {""}
+                          </span>
+                        )}
                       </span>
                     ))}
                   </span>
