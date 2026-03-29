9cca55bd08db719710929e313dc9f8a51c67fbf7
Marco Leipnitz <marco.leipnitz@gmx.net>
2026-03-29 17:14:23 +0200

    Remove references to the linus-app

--- Linus/testdata.sql
+++
@@ -1,4 +0,0 @@
--- password is hash of
---      Füsik!
---
-INSERT INTO `user` (id, name, roles, password, email) VALUES (1,'admin','[\"ROLE_ADMIN\"]','$argon2id$v=19$m=65536,t=4,p=1$L3l0bHJaYy9IUXZ3ZTIuag$iu4o17UPLPiZuzON1jHi9tAFqQGGlzdrrZ8H8yI+f6U','redacted@example.com');
